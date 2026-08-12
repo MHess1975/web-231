df1beb79ffb026f41b2cf233bc677bf7a9593e94
Mitch Hess <mihess@my365.bellevue.edu>
2026-08-12 16:01:38 -0500

    Week 1 Assignment Updates

---
+++ week-1/project01-02/project01-02.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author:Mitchell Hess
+    Date:August 12, 2026
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+let service1Speed = "0 MBPS";
+let service2Speed = "100 MBPS";
+let service3Speed = "500 MBPS";
+let service4Speed = "1 Gig";
